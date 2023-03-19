f003df7b3eae83258659df471db2ec25bf0b3b51
PaolaGraciella <paolagraciellafonkou@gmail.com>
2023-03-19 20:02:10 +0100

    yeahh

--- view/accueil.php
+++ view/accueil.php
@@ -1,18 +1,13 @@
 <?php 
 
-include ("../repositories/professeur.php");
-include ("../repositories/etudiant.php");
-
   session_start();
   if(empty($_SESSION["user"])){
     header("location:login.php");
   }
   if($_SESSION["role"]=="etudiant"){
-    $etudiant = new Etudiant();
     header('Location:accueil_etudiant.php');
   }
   else if($_SESSION["role"]=="professeur"){
-    $prof=new Professeur();
     header('Location:accueil_prof.php');
   }
 ?>
