35ad4038c8da6bb8c177616c560478931606ef46
PaolaGraciella <paolagraciellafonkou@gmail.com>
2023-03-19 21:21:49 +0100

    yes yes

--- view/accueil.php
+++ view/accueil.php
@@ -5,11 +5,9 @@
     header("location:login.php");
   }
   if($_SESSION["role"]=="etudiant"){
-    $etudiant = new Etudiant();
     header('Location:accueil_etudiant.php');
   }
   else if($_SESSION["role"]=="professeur"){
-    $prof=new Professeur();
     header('Location:accueil_prof.php');
   }
 ?>
