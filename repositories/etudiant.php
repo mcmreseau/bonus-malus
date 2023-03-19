e2efd3a1017318395bc130dc9be8b8d972448309
PaolaGraciella <paolagraciellafonkou@gmail.com>
2023-03-19 14:26:42 +0100

    voila

--- repositories/etudiant.php
+++ repositories/etudiant.php
@@ -17,5 +17,15 @@
             return $req->fetchAll();
         }
 
+        
+        public function findEtud(int $id_seance){
+            $stmt= " select e.nom_etudiant,e.mail,b.note from etudiant e
+                     LEFTER JOIN bonus b on e.id_etudiant = b.id_etudiant
+                     where b.id_seance=:id";
+            $req = $this->con->prepare($stmt);
+            $req->execute([":id"=>$id_seance]);
+            return $req->fetchAll();
+        }
 
+    }
 ?>
