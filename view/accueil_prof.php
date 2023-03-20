f8d1641cb36c9760e0108874956924753cbf864f
marcelle mazifa <mazifamm@3il.fr>
2023-03-20 09:53:33 +0100

    msg

--- view/accueil_prof.php
+++ view/accueil_prof.php
@@ -74,7 +74,6 @@
                 #Recuperation de la liste des séances
                   $seanceRepo = new SeanceRepo();
                   $seance = $seanceRepo->findSeanceNoActive($prof["id_prof"]);
-                
                   foreach($seance as $it){
                     ?>
                       <li class="px-2 py-1 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200">
